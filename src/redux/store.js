c936d4f5fd187b7ac6108a8c8f23a06b08361788
melchr <melchrstmn@gmail.com>
2020-10-30 22:56:31 -0400

    edited store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './rootReducer';
 
-const rootReducer = () => {
+const middlewares = [logger, thunk]
 
-}
-
-export default createStore(rootReducer, applyMiddleware)
+export default createStore(rootReducer, applyMiddleware( ... middlewares));
