c84234b51aae3f3f28caa39bfabfbba9aea31f18
melchr <melchrstmn@gmail.com>
2020-11-04 18:51:06 -0500

    added store and router to index file due to errors

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      HELLOOOO
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom'
+import reviewReducer from './reducers/reviewReducer'
 import './index.css';
 import App from './containers/App';
-import store from './redux/store';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reviewReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
