65423e7fa795448ad30cad50ed2c9900d5c6fd75
melchr <melchrstmn@gmail.com>
2020-11-05 00:38:16 -0500

    added reviewscontainer to app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import ReviewsContainer from './ReviewsContainer';
 
 const App = () => {
   return (
     <div className="App">
       <img class="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley"/>
       <h1>SPITE HOUSE</h1>
+      <br></br>
+      <ReviewsContainer />
     </div>
   );
 }
