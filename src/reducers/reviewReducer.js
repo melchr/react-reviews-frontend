96aac6b363efc15d2936be5230594bc60c508204
melchr <melchrstmn@gmail.com>
2020-11-04 23:50:35 -0500

    updated reducer for the fetch

--- src/reducers/reviewReducer.js
+++ src/reducers/reviewReducer.js
@@ -1,5 +1,7 @@
 export default function reviewReducer (state = {reviews: [], comments: []}, action) {
     switch (action.type) {
+        case 'REVIEWS_FETCH':
+            return {...state, reviews: action.payload}
         default:
             return state
     }
