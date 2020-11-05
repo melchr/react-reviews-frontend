1b6a18fa432546b9c7643544d257d5cec3b1a520
melchr <melchrstmn@gmail.com>
2020-11-05 00:31:56 -0500

    added/started building out reviews container

---
+++ src/containers/ReviewsContainer.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+import Reviews from '../components/reviews/Reviews'
+import { reviewsFetch } from '../actions/reviewsFetch'
+import { useDispatch, useSelector } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const ReviewsContainer = () => {
+
+
+export default ReviewsContainer
