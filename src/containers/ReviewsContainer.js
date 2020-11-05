8f1d663f48f5232626eaf8ead42eca6274cd4883
melchr <melchrstmn@gmail.com>
2020-11-05 00:37:43 -0500

    add Reviews component to container, connected fetch

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -5,6 +5,22 @@ import { useDispatch, useSelector } from 'react-redux'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const ReviewsContainer = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(reviewsFetch())
+    }, [])
 
+const reviews = useSelector(state => state.reviews)
+    
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route path='/reviews' render={(routerProps) => <Reviews {...routerProps} reviews={reviews}/>}/>
+                </Switch>
+            </Router>
+        </div>
+    )
+}
 
 export default ReviewsContainer
