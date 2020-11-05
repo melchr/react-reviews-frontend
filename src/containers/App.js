8393e55e9d6a72f5cc0403f3b9c9f91d64bef1a3
melchr <melchrstmn@gmail.com>
2020-11-04 21:15:25 -0500

    added logo, header

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,8 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      HELLOOOO
+      <img class="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley"/>
+      <h1>SPITE HOUSE</h1>
     </div>
   );
 }
