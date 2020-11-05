361f3dc465cb3ca4eed70ae187ad59f30869a1d0
melchr <melchrstmn@gmail.com>
2020-11-04 21:21:53 -0500

    added store and reducers to index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom'
 import reviewReducer from './reducers/reviewReducer'
 import './index.css';
 import App from './containers/App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reviewReducer, composeEnhancers(applyMiddleware(thunk)))
+
+const reducers = combineReducers({
+  reviewReducer
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
+  <Provider store={ store }>
       <App />
-    </Router>
   </Provider>,
   document.getElementById('root')
 );
