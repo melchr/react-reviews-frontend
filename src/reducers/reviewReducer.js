03a654bfa01433f58ac2f7234ae7fbfd558b023f
melchr <melchrstmn@gmail.com>
2020-11-04 21:43:02 -0500

    added review reducer, default function

--- src/reducers/reviewReducer.js
+++ src/reducers/reviewReducer.js
@@ -1,8 +1,8 @@
-export default function reviewReducer(state = {reviews: {}}, action) {
+export default function reviewReducer (state = {reviews: [], comments: []}, action) {
     switch (action.type) {
-        case 'REVIEWS_FETCH':
-            return { reviews: action.payload }
         default:
             return state
     }
 }
+
+//if only reducer, it needs to return as an object, not an array
