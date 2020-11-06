a830ecaca67f29d0e06e691a48b2ba2603f9bcdd
melchr <melchrstmn@gmail.com>
2020-11-06 09:31:58 -0500

    created header component for logo

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Header extends React.Component {
+
+    render() {
+      return (
+        <div>
+            <img className="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley"/>
+            <h1>SPITE HOUSE</h1>
+            <br></br>
+        </div>
+      );
+    }
+  }
+
+  export default Header
