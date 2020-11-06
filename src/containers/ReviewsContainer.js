7d3606dd09732eb88295902c8982f9dfce8e2d1e
melchr <melchrstmn@gmail.com>
2020-11-05 20:54:07 -0500

    edited state

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,26 +1,30 @@
-import React, { useEffect } from 'react'
-import Reviews from '../components/reviews/Reviews'
-import { reviewsFetch } from '../actions/reviewsFetch'
-import { useDispatch, useSelector } from 'react-redux'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { reviewsFetch } from "../actions/reviewsFetch.js";
+//import ReviewCard from "../components/reviews/Reviews";
 
-const ReviewsContainer = () => {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(reviewsFetch())
-    }, [])
 
-const reviews = useSelector(state => state.reviews)
-    
+class ReviewsContainer extends Component {
+
+  componentDidMount() {
+    this.props.reviewsFetch();
+  }
+  render() {
     return (
-        <div>
-            <Router>
-                <Switch>
-                    <Route path='/reviews' render={(routerProps) => <Reviews {...routerProps} reviews={reviews}/>}/>
-                </Switch>
-            </Router>
-        </div>
-    )
+      <div>
+
+      </div>
+    );
+  }
 }
 
-export default ReviewsContainer
+const mapStateToProps = state => {
+  return {
+    reviews: state.reviews.reviews
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { reviewsFetch }
+)(ReviewsContainer);
