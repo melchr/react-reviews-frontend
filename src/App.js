15c83a09689b1f3bda5454bf8e66cbde1b6e93f1
melchr <melchrstmn@gmail.com>
2020-11-06 09:32:19 -0500

    imported Header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 //import reviewsFetch from "./actions/reviewsFetch.js"
 import ReviewsContainer from './containers/ReviewsContainer';
 import ReviewList from './components/reviews/ReviewList'
+import Header from './components/Header'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
@@ -14,9 +15,8 @@ class App extends React.Component {
   return (
     <Router>
     <div className="App">
-      <img className="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley"/>
-      <h1>SPITE HOUSE</h1>
-      <br></br>
+      <Header />
+      <button>View All Reviews</button>
       <ReviewsContainer />
       {/* <ReviewList /> */}
       <Route exact path='/app/v1/reviews' component={ReviewList}></Route>
