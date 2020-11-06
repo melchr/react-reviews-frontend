a7b12d7087b9693538b8ba041e80e2ae9d86d742
melchr <melchrstmn@gmail.com>
2020-11-06 12:13:34 -0500

    changed review form reducer state

--- src/reducers/newReviewForm.js
+++ src/reducers/newReviewForm.js
@@ -8,7 +8,10 @@ const initialState = {
 export default (state=initialState, action) => {
     switch (action.type) {
         case "UPDATE_NEW_REVIEW_FORM":
-            return action.formData
+            return {
+                ...state,
+                [action.formData.name]: action.formData.value
+            }
         case "RESET_NEW_REVIEW_FORM":
             return initialState
         default:
