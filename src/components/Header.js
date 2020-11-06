b1107d7395f615c99263d3c30f77d8c178a20a77
melchr <melchrstmn@gmail.com>
2020-11-06 12:49:53 -0500

    adjusting logo size

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
       return (
         <div>
-            <img className="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley"/>
+            <img className="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley" width="200"/>
             <h1>SPITE HOUSE</h1>
             <br></br>
         </div>
