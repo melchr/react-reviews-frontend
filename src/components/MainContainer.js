3a53f35391a968c09ba2dcf15495155f4117d6ea
melchr <melchrstmn@gmail.com>
2020-11-05 20:57:07 -0500

    added container to house components

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReviewList from './reviews/ReviewList'
+
+const MainContainer = () => {
+    return (
+        <div className="MainContainer">
+            <ReviewList />
+        </div>
+    )
+}
+
+export default MainContainer
