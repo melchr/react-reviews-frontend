abe1d898045b3b839466d1878899ac64a022a1b8
melchr <melchrstmn@gmail.com>
2020-11-06 11:42:23 -0500

    added new review path

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import ReviewsContainer from './containers/ReviewsContainer';
 import ReviewList from './components/reviews/ReviewList'
+import NewReviewForm from './components/reviews/NewReviewForm'
 import Header from './components/Header'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends React.Component {
 
@@ -12,10 +13,11 @@ class App extends React.Component {
     <Router>
     <div className="App">
       <Header />
-      <button>View All Reviews</button>
+      <button><Link to="/">Home</Link></button>
+      <button><Link to="/app/v1/reviews">View All Reviews</Link></button>
       <ReviewsContainer />
-      {/* <ReviewList /> */}
       <Route exact path='/app/v1/reviews' component={ReviewList}></Route>
+      <Route exact path='/reviews/new' component={NewReviewForm}/>
     </div>
     </Router>
   );
