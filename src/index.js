f9b6609c8b6a1ba38f1ec1b0a94e9ae28e49a9e2
melchr <melchrstmn@gmail.com>
2020-11-05 20:56:16 -0500

    changed app file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './containers/App';
+import App from './App.js';
 import store from './store.js';
 
 ReactDOM.render(
