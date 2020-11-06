9e2e82bbc0f820f480932fa03b24b9aab71a9d93
melchr <melchrstmn@gmail.com>
2020-11-06 12:51:44 -0500

    updated review form

--- src/components/reviews/NewReviewForm.js
+++ src/components/reviews/NewReviewForm.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { updateNewReviewForm } from '../../actions/newReviewForm'
-
+import { connect } from 'react-redux'
 
 const NewReviewForm = () => {
 
     const handleChange = event => {
+        event.preventDefault()
         const { name, value } = event.target
         updateNewReviewForm(name, value)
     }
@@ -15,27 +16,39 @@ const NewReviewForm = () => {
             placeholder="Title"
             name="title"
             onChange={handleChange}
-            value={"title"}
+            //value={""}
         /><br/>
         <input
             placeholder="Content"
             name="content"
             onChange={handleChange}
-            value={"content"}
+            //value={""}
         /><br/>
         <input
             placeholder="Genre"
             name="genre"
             onChange={handleChange}
-            value={"genre"}
+            //value={""}
         /><br/>
         <input
             placeholder="Album Cover URL"
             name="img_url"
             onChange={handleChange}
-            value={"img_url"}
+            //value={""}
         />
+        <br/>
+        <input type="submit" value="Create Review"/>
     </form>
 )}
 
-export default NewReviewForm
+const mapStateToProps = state => {
+    const { title, content, genre, img_url } = state.newReviewForm
+    return {
+        title,
+        content,
+        genre,
+        img_url
+    }
+}
+
+export default connect(mapStateToProps, { updateNewReviewForm })(NewReviewForm)
