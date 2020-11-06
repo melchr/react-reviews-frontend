ec171de768211d970b6f793ce13e280bbebe656c
melchr <melchrstmn@gmail.com>
2020-11-06 14:40:54 -0500

    fixed ability to type in form

--- src/components/reviews/NewReviewForm.js
+++ src/components/reviews/NewReviewForm.js
@@ -2,39 +2,40 @@ import React from 'react'
 import { updateNewReviewForm } from '../../actions/newReviewForm'
 import { connect } from 'react-redux'
 
-const NewReviewForm = () => {
+const NewReviewForm = ({title, content, genre, img_url, updateNewReviewForm}) => {
 
     const handleChange = event => {
-        event.preventDefault()
         const { name, value } = event.target
         updateNewReviewForm(name, value)
     }
 
+    const handleSubmit = event => event.preventDefault()
+
     return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <input
             placeholder="Title"
             name="title"
             onChange={handleChange}
-            //value={""}
+            value={title}
         /><br/>
         <input
             placeholder="Content"
             name="content"
             onChange={handleChange}
-            //value={""}
+            value={content}
         /><br/>
         <input
             placeholder="Genre"
             name="genre"
             onChange={handleChange}
-            //value={""}
+            value={genre}
         /><br/>
         <input
             placeholder="Album Cover URL"
             name="img_url"
             onChange={handleChange}
-            //value={""}
+            value={img_url}
         />
         <br/>
         <input type="submit" value="Create Review"/>
