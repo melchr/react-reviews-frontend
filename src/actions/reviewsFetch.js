4b0cadd752605d43fea0fa0131e0e302e6268c79
melchr <melchrstmn@gmail.com>
2020-11-05 20:52:38 -0500

    added synchronous method

--- src/actions/reviewsFetch.js
+++ src/actions/reviewsFetch.js
@@ -1,5 +1,12 @@
+export const getReviews = reviews => {
+    return {
+        type: "GET_REVIEWS",
+        reviews
+    }
+} //action creator
+
+
 export const reviewsFetch = () => {
-    console.log("been fetched")
     return dispatch => {
         fetch('http://localhost:3000/api/v1/reviews', {
             method: "GET",
@@ -8,9 +15,6 @@ export const reviewsFetch = () => {
             }
         })
         .then(response => response.json())
-        .then(reviews => dispatch ({
-            type: 'REVIEWS_FETCH',
-            payload: reviews.data
-        }))
+        .then(reviews => dispatch (getReviews(reviews.data)))
     }
 }
