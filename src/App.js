a19ca6823a87c4ec77530980c2d97a531b3ae041
melchr <melchrstmn@gmail.com>
2020-11-06 09:52:25 -0500

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-//import reviewsFetch from "./actions/reviewsFetch.js"
 import ReviewsContainer from './containers/ReviewsContainer';
 import ReviewList from './components/reviews/ReviewList'
 import Header from './components/Header'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
-  componentDidMount() {
-    //this.props.reviewsFetch()
-  }
 
   render(){
   return (
