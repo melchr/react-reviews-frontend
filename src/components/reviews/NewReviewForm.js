d8cebda3ff79fafe3e29f7f96d89519bd7e0f4d1
melchr <melchrstmn@gmail.com>
2020-11-06 12:13:04 -0500

    updating new review form

--- src/components/reviews/NewReviewForm.js
+++ src/components/reviews/NewReviewForm.js
@@ -1,25 +1,36 @@
 import React from 'react'
+import { updateNewReviewForm } from '../../actions/newReviewForm'
+
 
 const NewReviewForm = () => {
-    const handleChange=()=>{}
+
+    const handleChange = event => {
+        const { name, value } = event.target
+        updateNewReviewForm(name, value)
+    }
+
     return (
     <form>
         <input
+            placeholder="Title"
             name="title"
             onChange={handleChange}
             value={"title"}
-        />
+        /><br/>
         <input
+            placeholder="Content"
             name="content"
             onChange={handleChange}
             value={"content"}
-        />
+        /><br/>
         <input
+            placeholder="Genre"
             name="genre"
             onChange={handleChange}
             value={"genre"}
-        />
+        /><br/>
         <input
+            placeholder="Album Cover URL"
             name="img_url"
             onChange={handleChange}
             value={"img_url"}
