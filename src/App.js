87a4856db756ede655163f9229e990711ddad6be
melchr <melchrstmn@gmail.com>
2020-11-05 21:57:50 -0500

    added router for reviews

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 //import reviewsFetch from "./actions/reviewsFetch.js"
 import ReviewsContainer from './containers/ReviewsContainer';
 import ReviewList from './components/reviews/ReviewList'
-//import ReviewCard from './components/reviews/ReviewCard'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
   componentDidMount() {
@@ -12,13 +12,16 @@ class App extends React.Component {
 
   render(){
   return (
+    <Router>
     <div className="App">
       <img className="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley"/>
       <h1>SPITE HOUSE</h1>
       <br></br>
       <ReviewsContainer />
-      <ReviewList />
+      {/* <ReviewList /> */}
+      <Route exact path='/app/v1/reviews' component={ReviewList}></Route>
     </div>
+    </Router>
   );
   }
 }
