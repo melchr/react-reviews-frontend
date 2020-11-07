79f9dde6c25c299920da75b201c91711ab607f9f
melchr <melchrstmn@gmail.com>
2020-11-07 10:47:24 -0500

    moved browser router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App.js';
 import store from './store.js';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <Provider store={ store }>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
