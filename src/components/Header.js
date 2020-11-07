4dd57f0bfd3116ace0a1f355cd0929e7c0c61712
melchr <melchrstmn@gmail.com>
2020-11-07 10:46:26 -0500

    added links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
 
 class Header extends React.Component {
 
@@ -8,6 +9,9 @@ class Header extends React.Component {
             <img className="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley" width="200"/>
             <h1>SPITE HOUSE</h1>
             <br></br>
+            <NavLink exact to="/">Home</NavLink> |
+            <NavLink exact to="/app/v1/reviews">View All Reviews</NavLink> | 
+            <NavLink exact to="/app/v1/reviews/new">New Review</NavLink>
         </div>
       );
     }
