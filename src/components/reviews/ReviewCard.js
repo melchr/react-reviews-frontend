05a3273dfbf343b94f9e9e3073d9b01424704509
melchr <melchrstmn@gmail.com>
2020-11-07 10:47:06 -0500

    removed content

--- src/components/reviews/ReviewCard.js
+++ src/components/reviews/ReviewCard.js
@@ -4,7 +4,7 @@ const ReviewCard = ({ review }) => {
   return (
     <div>
     <p><strong>{review.attributes.title}</strong></p>
-    <p>{review.attributes.content}</p>
+    {/* <p>{review.attributes.content}</p> */}
     <br></br>
     </div>
   )
