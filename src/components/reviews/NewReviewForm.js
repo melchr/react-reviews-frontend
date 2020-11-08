5e02399fa20085b06d8d5f4293a614876f7fe731
melchr <melchrstmn@gmail.com>
2020-11-08 08:53:53 -0500

    visual edits

--- src/components/reviews/NewReviewForm.js
+++ src/components/reviews/NewReviewForm.js
@@ -19,6 +19,8 @@ const NewReviewForm = ({ formData, updateNewReviewForm, createReview }) => {
     }
 
     return (
+        <div>
+        <br></br>
     <form onSubmit={handleSubmit}>
         <input
             placeholder="Title"
@@ -47,6 +49,7 @@ const NewReviewForm = ({ formData, updateNewReviewForm, createReview }) => {
         <br/>
         <input type="submit" value="Create Review"/>
     </form>
+    </div>
 )}
 
 const mapStateToProps = state => {
