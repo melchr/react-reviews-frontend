a1aca5143b0c77445e9f0eef5c218a41d1bc1279
melchr <melchrstmn@gmail.com>
2020-11-08 15:41:36 -0500

    added navlink

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,11 @@ class Header extends React.Component {
             <img className="App-logo" src="https://i.ibb.co/fXg9HgB/2-22657-smiley-face-black-and-white-smiley-face-clipart.png" alt="spite-house-smiley" width="200"/>
             <h1>SPITE HOUSE</h1>
             <br></br>
-            <NavLink exact to="/">Home</NavLink> |
-            <NavLink exact to="/app/v1/reviews">View All Reviews</NavLink> | 
-            <NavLink exact to="/app/v1/reviews/new">New Review</NavLink>
+            <strong>
+            <NavLink className="App-link" exact to="/">Home</NavLink> |
+            <NavLink className="App-link" exact to="/app/v1/reviews">View All Reviews</NavLink> | 
+            <NavLink className="App-link" exact to="/app/v1/reviews/new">New Review</NavLink>
+            </strong>
         </div>
       );
     }
