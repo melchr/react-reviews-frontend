277ca3cb95b3ff22226d6107a3567d21cd1982a2
melchr <melchrstmn@gmail.com>
2020-11-08 15:43:41 -0500

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component {
+
+    render() {
+      return (
+        <div className="Footer">
+            <p>Spite House &copy;2020</p>
+        </div>
+      );
+    }
+  }
+
+  export default Footer
