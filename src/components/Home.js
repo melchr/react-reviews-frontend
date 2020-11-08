39a5ddaa203d7db85477cfb5030b8758db506ec9
melchr <melchrstmn@gmail.com>
2020-11-08 15:44:17 -0500

    added home component

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Home extends React.Component {
+
+    render() {
+      return (
+        <div className="Home">
+            <br></br><br></br>
+            SPITE HOUSE was created on the basis of not being forced to read positive reviews of albums I didn't agree with, and is instead a similar forceful rendition of the exact opposite.
+            <br></br><br></br>
+        </div>
+      );
+    }
+  }
+
+  export default Home
